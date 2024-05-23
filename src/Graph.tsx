7edb318e021417c9296576c843adf69e7327a895
Jash Kadakia <73967429+jashkad8967@users.noreply.github.com>
2024-05-23 13:44:38 -0500

    Final commit 6

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -39,7 +39,7 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('columns', '["ratio, lower_bound, upper_bound, trigger_alert"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('aggregates', JSON.stringify({
         price_abc: 'avg',
